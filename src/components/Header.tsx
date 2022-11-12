dcecbeb571c0cd208fde6e24a65cec29fecc432e
unknown <omarramirez5876@gmail.com>
2022-11-12 13:32:34 -0500

    auth with firebase

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import { RiShoppingBasketFill } from "react-icons/ri";
 import Logo from "../img/logo.png";
 import Avatar from "../img/avatar.png";
-import {motion} from 'framer-motion'
-import {Link} from 'react-router-dom'
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { app } from '../../Firebase.config';
 
 const Header = () => {
+  const firebaseAuth = getAuth(app)
+  const provider = new GoogleAuthProvider();
+  const login = async() => {
+    const response = await signInWithPopup(firebaseAuth, provider);
+    console.log(response)
+  }
   return (
     <header className="fixed z-50 w-screen p-6 px-16">
       {/* desktop */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
-        <Link to={'/'} className="flex items-center gap-2">
+        <Link to={"/"} className="flex items-center gap-2">
           <img src={Logo} className="w-8 object-cover" alt="logo" />
           <p className="text-gray-800  text-xl font-bold">City</p>
         </Link>
@@ -37,8 +45,15 @@ const Header = () => {
               <p className="text-xs text-white font-semibold">2</p>
             </div>
           </div>
-
-          <motion.img whileTap={{scale: 0.6}} src={Avatar} className='w-10 min-w-[40px] h-10 min-h-[40px] shadow-xl cursor-pointer' alt="avatar" />
+          <div className="relative">
+            <motion.img
+              whileTap={{ scale: 0.6 }}
+              src={Avatar}
+              className="w-10 min-w-[40px] h-10 min-h-[40px] shadow-xl cursor-pointer"
+              alt="avatar"
+              onClick={login}
+            />
+          </div>
         </div>
       </div>
 
